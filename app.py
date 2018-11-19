2ee520f15bdeda42783785d7f1da740c4e4a7d0c
Your Name <you@example.com>
2018-11-20 01:28:18 +0700

    flask_sqlalchemyの導入とユーザーモデルとノートモデルの実装

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
+import os
+
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "noter.db")
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    content = db.Column(db.Text)
+    last_modified = db.Column(db.DateTime)
+
 
 @app.route('/')
 def index():
